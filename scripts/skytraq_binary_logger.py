4f169a88967dfe81f5ff525aae3524a3d8143215
ryanpdx <rmedick@pdx.edu>
2021-04-14 16:21:19 -0700

    removed ACK assert

--- scripts/skytraq_binary_logger.py
+++ scripts/skytraq_binary_logger.py
@@ -29,14 +29,13 @@ def main():
     # open serial
     ser = Serial('/dev/ttyS2', 115200, timeout=5.0)
 
-    # swap to binary mode and assert ACK
+    # swap to binary mode
     ser.write(b'\xA0\xA1\x00\x03\x09\x02\x00\x0B\x0D\x0A')
     try:
         line = ser.readline()
     except SerialException as exc:
         print('Device error: {}\n'.format(exc))
         sys.exit(1)
-    assert line == b'\xa0\xa1\x00\x02\x83\t\x8a\r\n'
 
     while 1:
         try:
