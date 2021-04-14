46fe925bca87057fab6d15d506b437283c0612bf
ryanpdx <rmedick@pdx.edu>
2021-04-14 16:09:25 -0700

    add ^C catch and datetime to scripts

--- scripts/skytraq_binary.py
+++ scripts/skytraq_binary.py
@@ -5,8 +5,11 @@ import struct
 from serial import Serial, SerialException
 
 # first time will fail
-with open("/sys/class/gpio/export", "w") as fptr:
-    fptr.write("98")
+try:
+    with open("/sys/class/gpio/export", "w") as fptr:
+        fptr.write("98")
+except PermissionError:
+    pass
 with open("/sys/class/gpio/export", "w") as fptr:
     fptr.write("98")
 with open("/sys/class/gpio/gpio98/direction", "w") as fptr:
@@ -14,30 +17,42 @@ with open("/sys/class/gpio/gpio98/direction", "w") as fptr:
 with open("/sys/class/gpio/gpio98/value", "w") as fptr:
     fptr.write("1")
 
-# open serial
-ser = Serial('/dev/ttyS2', 115200, timeout=5.0)
 
-# swap to binary mode and assert ACK
-ser.write(b'\xA0\xA1\x00\x03\x09\x02\x00\x0B\x0D\x0A')
-try:
-    line = ser.readline()
-except SerialException as exc:
-    print('Device error: {}\n'.format(exc))
-    sys.exit(1)
-assert line == b'\xa0\xa1\x00\x02\x83\t\x8a\r\n'
+def main():
+    """loop and print data"""
 
-while 1:
+    # open serial
+    ser = Serial('/dev/ttyS2', 115200, timeout=5.0)
+
+    # swap to binary mode and assert ACK
+    ser.write(b'\xA0\xA1\x00\x03\x09\x02\x00\x0B\x0D\x0A')
     try:
         line = ser.readline()
     except SerialException as exc:
         print('Device error: {}\n'.format(exc))
-        break
+        sys.exit(1)
+    assert line == b'\xa0\xa1\x00\x02\x83\t\x8a\r\n'
 
-    try:
-        data = struct.unpack('=4x3BHI2i2I5H6i3x', line)
-    except struct.error as exc:
-        print('Parse error: {}\n'.format(exc))
-        continue
+    while 1:
+        try:
+            line = ser.readline()
+        except SerialException as exc:
+            print('Device error: {}\n'.format(exc))
+            break
 
-    print(data)
-    print("")
+        try:
+            data = struct.unpack('=4x3BHI2i2I5H6i3x', line)
+        except struct.error as exc:
+            print('Parse error: {}\n'.format(exc))
+            continue
+
+        print(data)
+        print("")
+
+
+try:
+    main()
+except KeyboardInterrupt:
+    with open("/sys/class/gpio/gpio98/value", "w") as fptr:
+        fptr.write("0")
+    sys.exit()
--- scripts/skytraq_binary_logger.py
+++ scripts/skytraq_binary_logger.py
@@ -1,11 +1,15 @@
 """Log skytraq binary data"""
 
 import sys
+from datetime import datetime
 from serial import Serial, SerialException
 
 # first time will fail
-with open("/sys/class/gpio/export", "w") as fptr:
-    fptr.write("98")
+try:
+    with open("/sys/class/gpio/export", "w") as fptr:
+        fptr.write("98")
+except PermissionError:
+    pass  # first time will fail
 with open("/sys/class/gpio/export", "w") as fptr:
     fptr.write("98")
 with open("/sys/class/gpio/gpio98/direction", "w") as fptr:
@@ -14,27 +18,38 @@ with open("/sys/class/gpio/gpio98/value", "w") as fptr:
     fptr.write("1")
 
 # open data file
-fptr = open("data.txt", "ab")
+time_str = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+fptr = open("/home/oresat/skytraq_data_" + time_str + ".txt", "wb")
 
-# open serial
-ser = Serial('/dev/ttyS2', 115200, timeout=5.0)
 
-# swap to binary mode and assert ACK
-ser.write(b'\xA0\xA1\x00\x03\x09\x02\x00\x0B\x0D\x0A')
-try:
-    line = ser.readline()
-except SerialException as exc:
-    print('Device error: {}\n'.format(exc))
-    sys.exit(1)
-assert line == b'\xa0\xa1\x00\x02\x83\t\x8a\r\n'
+def main():
+    """loop and print data"""
 
-while 1:
+    # open serial
+    ser = Serial('/dev/ttyS2', 115200, timeout=5.0)
+
+    # swap to binary mode and assert ACK
+    ser.write(b'\xA0\xA1\x00\x03\x09\x02\x00\x0B\x0D\x0A')
     try:
         line = ser.readline()
     except SerialException as exc:
         print('Device error: {}\n'.format(exc))
+        sys.exit(1)
+    assert line == b'\xa0\xa1\x00\x02\x83\t\x8a\r\n'
+
+    while 1:
+        try:
+            line = ser.readline()
+        except SerialException as exc:
+            print('Device error: {}\n'.format(exc))
         break
 
     fptr.write(line)
 
-fptr.close()
+
+try:
+    main()
+except KeyboardInterrupt:
+    fptr.close()
+    with open("/sys/class/gpio/gpio98/value", "w") as fptr:
+        fptr.write("0")
