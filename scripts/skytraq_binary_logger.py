79064e1c3c08915ff7f6838a345eac32e33c3444
ryanpdx <rmedick@pdx.edu>
2021-04-23 18:29:47 -0700

    script now use io.TextIOWrapper

--- scripts/skytraq_binary_logger.py
+++ scripts/skytraq_binary_logger.py
@@ -3,6 +3,7 @@
 import sys
 from datetime import datetime
 import struct
+import io
 from serial import Serial, SerialException
 
 # open data file
@@ -56,15 +57,17 @@ def main():
     # swap to binary mode
     ser.write(b'\xA0\xA1\x00\x03\x09\x02\x00\x0B\x0D\x0A')
 
+    sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser), newline='\r\n')
+
     try:
-        line = ser.readline()
+        line = sio.readline()
     except SerialException as exc:
         print('Device error: {}\n'.format(exc))
         sys.exit(1)
 
     while 1:
         try:
-            line = ser.readline()
+            line = sio.readline()
         except SerialException as exc:
             print('Device error: {}\n'.format(exc))
             continue
