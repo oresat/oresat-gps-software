374a639835518b3f2111b4487bcaa39f67fd48f6
ryanpdx <rmedick@protonmail.com>
2023-04-15 18:26:35 -0700

    update for olaf v2

--- oresat_gps/__main__.py
+++ oresat_gps/__main__.py
@@ -1,23 +1,28 @@
 import os
 
-from flask import Blueprint, render_template
-from olaf import app, rest_api, olaf_run
+from olaf import app, rest_api, olaf_setup, olaf_run, render_olaf_template
 
 from .gps_resource import GPSResource
 
-gps_bp = Blueprint('gps_template', __name__, template_folder='templates')
 
-
-@gps_bp.route('/skytraq')
+@rest_api.app.route('/skytraq')
 def skytraq_template():
-    return render_template('skytraq.html', title=os.uname()[1], name='SkyTraq')
+    return render_olaf_template('skytraq.html', name='SkyTraq')
 
 
 def main():
-    app.add_resource(GPSResource)
-    rest_api.add_blueprint(gps_bp)
 
-    olaf_run(f'{os.path.dirname(os.path.abspath(__file__))}/data/oresat_gps.dcf')
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    args = olaf_setup(f'{path}/data/oresat_gps.dcf')
+    mock_args = [i.lower() for i in args.mock_hw]
+    mock_skytraq = 'skytraq' in mock_args or 'all' in mock_args
+
+    app.add_resource(GPSResource(mock_skytraq))
+
+    rest_api.add_template(f'{path}/templates/skytraq.html')
+
+    olaf_run()
 
 
 if __name__ == '__main__':
--- oresat_gps/gps_resource.py
+++ oresat_gps/gps_resource.py
@@ -2,9 +2,8 @@ from os import geteuid
 from enum import IntEnum
 from time import clock_settime, CLOCK_REALTIME
 
-from olaf import Resource, scet_int_from_time, logger
+from olaf import Resource, scet_int_from_time, logger, GPIO
 
-from .gpio import GPIO
 from .skytraq import SkyTraq, NavData, FixMode, gps_datetime
 
 
@@ -29,13 +28,17 @@ class GPSResource(Resource):
     INDEX_SKYTRAQ_CONTROL = 0x6000
     INDEX_SKYTRAQ_DATA = 0x6001
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, mock_skytraq: bool):
+        super().__init__()
 
-        self._gpio0 = None
-        self._gpio1 = None
+        self._gpio_skytraq = None
+        self._gpio_lna = None
         self._skytraq = None
         self._state = States.OFF
+        self._mock_skytraq = mock_skytraq
+
+        if mock_skytraq:
+            logger.warning('mocking SkyTraq and GPIO power pins')
 
         self._uid = geteuid()
         if self._uid != 0:
@@ -43,30 +46,29 @@ class GPSResource(Resource):
 
     def on_start(self):
 
-        self.control_rec = self.od[self.INDEX_SKYTRAQ_CONTROL]
-        self.data_rec = self.od[self.INDEX_SKYTRAQ_DATA]
+        self.control_rec = self.node.od[self.INDEX_SKYTRAQ_CONTROL]
+        self.data_rec = self.node.od[self.INDEX_SKYTRAQ_DATA]
+
+        self.node.add_sdo_read_callback(self.INDEX_SKYTRAQ_CONTROL, self._on_read)
+        self.node.add_sdo_write_callback(self.INDEX_SKYTRAQ_CONTROL, self._on_write)
+
+        self.control_rec[ControlSubindex.MOCK.value].value = self._mock_skytraq
 
         # control subindexes
-        self.mock_obj = self.control_rec[ControlSubindex.MOCK.value]
         self.is_syncd_obj = self.control_rec[ControlSubindex.IS_SYNCD.value]
         self.status_obj = self.control_rec[ControlSubindex.STATUS.value]
 
         # make sure the flag for the time has been syncd is set to false
         self.is_syncd_obj.value = False
 
-        self.mock_obj.value = self.mock_hw
-        if self.mock_hw:
-            logger.warning('mocking SkyTraq')
-        else:
-            skytraq_pin = self.control_rec[ControlSubindex.SKYTRAQ_PIN.value].value
-            lna_pin = self.control_rec[ControlSubindex.LNA_PIN.value].value
-            self._gpio_skytraq = GPIO(skytraq_pin)
-            self._gpio_lna = GPIO(lna_pin)
-            self._skytraq_power_on()
-
+        skytraq_pin = self.control_rec[ControlSubindex.SKYTRAQ_PIN.value].value
+        lna_pin = self.control_rec[ControlSubindex.LNA_PIN.value].value
+        self._gpio_skytraq = GPIO(skytraq_pin, self._mock_skytraq)
+        self._gpio_lna = GPIO(lna_pin, self._mock_skytraq)
         self._skytraq_power_on()
+
         serial_bus = self.control_rec[ControlSubindex.SERIAL_BUS.value].value
-        self._skytraq = SkyTraq(serial_bus, self._new_message, self._new_error, self.mock_hw)
+        self._skytraq = SkyTraq(serial_bus, self._new_message, self._new_error, self._mock_skytraq)
         self._skytraq.start()
         self._state = States.SEARCHING
 
@@ -75,15 +77,16 @@ class GPSResource(Resource):
         self._skytraq_power_off()
         self._state = States.OFF
 
-    def on_read(self, index, subindex, od):
+    def _on_read(self, index: int, subindex: int):
+
         if index == self.INDEX_SKYTRAQ_CONTROL and subindex == ControlSubindex.STATUS:
             return self._state.value
 
-    def on_write(self, index, subindex, od, data):
+    def _on_write(self, index: int, subindex: int, value):
 
         if index == self.INDEX_SKYTRAQ_CONTROL and subindex == ControlSubindex.STATUS:
             # turn skytraq on/off
-            if od.decode_raw(data):
+            if value:
                 self._skytraq_power_on()
             else:
                 self._skytraq_power_off()
@@ -109,10 +112,10 @@ class GPSResource(Resource):
             self.data_rec[0x14].value = scet_int_from_time(dt)
 
             # send gps tpdos
-            self.send_tpdo(2)
-            self.send_tpdo(3)
-            self.send_tpdo(4)
-            self.send_tpdo(5)
+            self.node.send_tpdo(2)
+            self.node.send_tpdo(3)
+            self.node.send_tpdo(4)
+            self.node.send_tpdo(5)
 
         # update status
         if nav_data.number_of_sv >= 4 and nav_data.fix_mode >= FixMode.FIX_2D:
@@ -128,15 +131,13 @@ class GPSResource(Resource):
     def _skytraq_power_on(self):
 
         logger.info('turning SkyTraq on')
-        if not self.mock_hw:
-            self._gpio_skytraq.on()
-            self._gpio_lna.on()
+        self._gpio_skytraq.high()
+        self._gpio_lna.high()
         self._state = States.SEARCHING
 
     def _skytraq_power_off(self):
 
         logger.info('turning SkyTraq off')
-        if not self.mock_hw:
-            self._gpio_skytraq.off()
-            self._gpio_lna.off()
+        self._gpio_skytraq.low()
+        self._gpio_lna.low()
         self._state = States.OFF
