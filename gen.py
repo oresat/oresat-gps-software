d041012ae124719b35ca90c59590af55ceb0a798
Ryan Medick <rmedick@protonmail.com>
2025-05-23 21:28:38 -0700

    update gen script and pyproject.toml for od.csv changes

--- gen.py
+++ gen.py
@@ -3,20 +3,26 @@
 import shutil
 from argparse import ArgumentParser
 
-from oresat_configs import gen_cand_files, gen_cand_od_config, gen_dbc_node
+from oresat_configs import gen_cand_files, gen_dbc_node
 
 OD_CONFIG_PATH = "od.yaml"
 GEN_DIR_PATH = "oresat_gps/gen"
 
 parser = ArgumentParser()
-parser.add_argument("gen", nargs="?", choices=["code", "config", "dbc", "clean"], default="code")
+subparsers = parser.add_subparsers(dest="subcommand")
+
+subparser = subparsers.add_parser("code", help="generate code for project")
+
+subparser = subparsers.add_parser("dbc", help="generate dbc file")
+subparser.add_argument("-n", "--node-id", default=0x7C)
+
+subparser = subparsers.add_parser("clean", help="clean up generated code")
+
 args = parser.parse_args()
 
-if args.gen == "code":
+if args.subcommand in ("code", None):
     gen_cand_files(OD_CONFIG_PATH, GEN_DIR_PATH)
-if args.gen == "config":
-    gen_cand_od_config(OD_CONFIG_PATH)
-elif args.gen == "dbc":
-    gen_dbc_node(OD_CONFIG_PATH, node_id=0x34)
-elif args.gen == "clean":
+elif args.subcommand == "dbc":
+    gen_dbc_node(OD_CONFIG_PATH, node_id=args.node_id)
+elif args.subcommand == "clean":
     shutil.rmtree(GEN_DIR_PATH, ignore_errors=True)
